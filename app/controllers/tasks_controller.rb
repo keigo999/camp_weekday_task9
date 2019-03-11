03168e8493fe97c23bf94e85216f68b59e317d0f
keigo999 <highbridgek5@gmail.com>
2019-03-11 12:12:28 +0900

    登録機能の追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  include TasksHelper
+
   def index
     @tasks = Task.all
   end
@@ -11,9 +13,22 @@ class TasksController < ApplicationController
   end
 
   def show
+    @task = Task.find(params[:id])
+    status_confirmation(@task)
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to @task
   end
   
   def hide
   end
-  
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :memo, :status)
+  end  
 end
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,13 @@
 module TasksHelper
+
+  def status_confirmation(task)
+    @status =   case task.status
+                when "0"
+                  "draft"
+                when "1"
+                  "published"
+                when "2"
+                  "archieved"
+                end
+  end
 end
