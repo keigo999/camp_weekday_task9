b43f7f88558e2ced8b4b4661a7ca2b01aa39f02a
keigo999 <highbridgek5@gmail.com>
2019-03-11 16:21:39 +0900

    削除後のルーティング設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,6 +21,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
+      flash[:notice] = "タスクを登録しました。"
       redirect_to @task
     else
       render "new"
