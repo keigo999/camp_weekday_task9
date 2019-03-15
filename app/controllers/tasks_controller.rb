0ec302263d027b2f9c77f8368a112efd33e06bce
keigo999 <highbridgek5@gmail.com>
2019-03-15 21:51:53 +0900

    リファクタリング

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_up, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.order(created_at: :desc).limit(5)
+    @tasks = Task.where(is_display: true).order(created_at: :desc).limit(5)
   end
 
   def new
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,5 @@ class Task < ApplicationRecord
   validates :memo, presence: true, length: { maximum:30, minimum:10 }
 
   #enumを使いDBには整数を保存して、表示するときには文字列にする
-  enum status: {draft: 0, published: 1, archieved: 2}
+  enum status: { draft: 0, published: 1, archieved: 2 }
 end
